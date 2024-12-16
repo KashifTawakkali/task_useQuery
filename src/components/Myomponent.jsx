48b872dc60fbab0ddaf7bdb67868254e6a891ede
glpat-JeRBose9xiehRzy1yF6P <patelkashif08@gmail.com>
2024-12-16 15:41:30 +0530

    change in url

--- src/components/Myomponent.jsx
+++ src/components/Myomponent.jsx
@@ -12,13 +12,14 @@ const MyComponent = () => {
         setLoading(true);
         const result = await fetchData({
           method: 'GET',
-          path: 'https://bayut-org-testing.mini91.com/',
+          path: 'https://enquiry-management-vista-enquiry-api.yn6km5.easypanel.host/api/enquiries?p=0&n=8&s=&k=&a=&ws=',
           query: {},
           options: {
             headers: {
-              Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NWZkNTVkYTY4NGMzNjZiYmIwZThjOCIsImlhdCI6MTczNDMzMzgzMCwiZXhwIjoxNzM0NDIwMjMwfQ.AXAP8-73ZeE3WE0dRxyQXMsc1H46COdv1tBxMGloF8c`,
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MDYzMTg3NTRkYTg3NmM2YTUxNTc4MyIsImlzU3VwZXJBZG1pbiI6dHJ1ZSwiaWF0IjoxNzM0MzQzNTY1LCJleHAiOjE3MzQ0Mjk5NjV9.kar7rFjI0GTqSQ65xnOaajGhZZtNTFvuefTsK0GquQI`,
               'Content-Type': 'application/json',
-            },
+            }
+            
           },
         });
         setData(result);
